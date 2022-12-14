a974d360c2a261eeef14ff333304085792934e78
Nidalee98 <tjdgh-0313@kakao.com>
2022-12-14 16:53:07 +0900

    chapter2 08 문자열 결합

---
+++ chapter2/src/chapter2_8/Ex2_7.java
@@ -0,0 +1,28 @@
+package chapter2_8;
+
+public class Ex2_7 {
+    public static void main(String[] args) {
+        String name = "Ja" + "va";
+        String str = "name" + 8.0;
+
+        System.out.println(name);
+        System.out.println(str);
+        System.out.println(7 + " ");
+        System.out.println(" " + 7);
+        System.out.println(7 + "");
+        System.out.println("" + 7);
+        System.out.println("" + "");
+        System.out.println(7 + 7 + "");
+        System.out.println("" + 7 + 7);
+    }
+}
+//실행결과
+//Java
+//name8.0
+//7 
+// 7
+//7
+//7
+//
+//14
+//77

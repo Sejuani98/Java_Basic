bd047dd47b5606ec2d495b608af7d52f57bb1576
Nidalee98 <tjdgh-0313@kakao.com>
2022-12-14 16:40:04 +0900

    chapter2 02 덧셈 뺄셈 계산하기

---
+++ chapter2/src/chapter2_2/Ex2_3.java
@@ -0,0 +1,10 @@
+package chapter2_2;
+
+public class Ex2_3 {
+    public static void main(String[] args) {
+        System.out.println(5 + 3); // 8
+        System.out.println(5 - 3); // 2
+        System.out.println(5 * 3); // 15
+        System.out.println(5 / 3); // 1
+    }
+}

70bbc0e2328d32a38798708d7f65008a902881f6
Nidalee98 <tjdgh-0313@kakao.com>
2022-12-14 16:37:19 +0900

    chapter2 01 화면에 글자 출력하기 - print()과 println()

---
+++ chapter2/src/chapter2_1/Ex2_1.java
@@ -0,0 +1,12 @@
+package chapter2_1;
+
+public class Ex2_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");//화면에 Hello, world를 출력하고 줄바꿈 o
+        System.out.print("Hello"); //화면에 Hello를 출력하고 줄바꿈 x
+        System.out.println("World"); //화면에 World를 출력하고 줄바꿈 o
+    }
+}
+//출력 결과
+//Hello, world
+//HelloWorld
---
+++ chapter2/src/chapter2_1/Ex2_2.java
@@ -0,0 +1,13 @@
+package chapter2_1;
+
+public class Ex2_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");//화면에 Hello, world가 출력된다
+        System.out.print("3 + 5 = ");//화면에 3 + 5 = 를 출력하고 줄바꿈 x
+        System.out.println(3 + 5);//화면에 8 출력
+    }
+}
+
+//실행결과
+//Hello, world
+//3 + 5 = 8

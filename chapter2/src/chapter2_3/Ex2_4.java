ca8087873ceb9dcb3bfcd27889dc1511c65784ab
Nidalee98 <tjdgh-0313@kakao.com>
2022-12-14 16:45:28 +0900

    chapter2 03 변수의 선언과 저장

---
+++ chapter2/src/chapter2_3/Ex2_4.java
@@ -0,0 +1,11 @@
+package chapter2_3;
+
+public class Ex2_4 {
+    public static void main(String[] args) {
+        int x = 5; //int x; 와 x = 5;를 이처럼 한줄로 합칠 수 있다.
+        System.out.println(x); // 화면에 x 값인 5가 출력된다
+
+        x = 10; //변수 x에 10을 저장. 기존에 저장된 5는 사라짐
+        System.out.println(x); // 화면에 x 값인 10이 출력된다
+    }
+}
